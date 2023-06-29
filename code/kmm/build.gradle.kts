fb5d91381389107c1e550c4550bc433fdf06a420
askantz <anna_skantz@hotmail.com>
2023-06-29 15:06:58 +0200

    Thesis project

---
+++ code/kmm/build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    //trick: for the same plugin versions in all sub-modules
+    id("com.android.application").version("7.4.0").apply(false)
+    id("com.android.library").version("7.4.0").apply(false)
+    kotlin("android").version("1.8.0").apply(false)
+    kotlin("multiplatform").version("1.8.0").apply(false)
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
+
+buildscript {
+    dependencies {
+        classpath("app.cash.sqldelight:gradle-plugin:2.0.0-alpha05")
+    }
+}
