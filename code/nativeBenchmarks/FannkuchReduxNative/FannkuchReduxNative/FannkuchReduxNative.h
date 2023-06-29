6cc317b10c4d00d66125efa8d0e1fb13a58c372b
askantz <anna_skantz@hotmail.com>
2023-06-29 15:02:36 +0200

    Thesis project

---
+++ code/nativeBenchmarks/FannkuchReduxNative/FannkuchReduxNative/FannkuchReduxNative.h
@@ -0,0 +1,18 @@
+//
+//  FannkuchReduxNative.h
+//  FannkuchReduxNative
+//
+//  Created by Anna Skantz on 2023-04-06.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FannkuchReduxNative.
+FOUNDATION_EXPORT double FannkuchReduxNativeVersionNumber;
+
+//! Project version string for FannkuchReduxNative.
+FOUNDATION_EXPORT const unsigned char FannkuchReduxNativeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FannkuchReduxNative/PublicHeader.h>
+
+
